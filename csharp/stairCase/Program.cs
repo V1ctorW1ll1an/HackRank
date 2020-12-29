c454275130adaee0ef2aa165972aa157a5fcd854
Victor Willian de Sousa <victorwillian1012@gmail.com>
2020-12-29 16:12:51 -0300

    stairCase challenge

---
+++ csharp/stairCase/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+            Console.WriteLine(new String('#', i + 1).PadLeft(N, ' '));
+    }
+}
