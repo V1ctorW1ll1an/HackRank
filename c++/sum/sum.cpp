1eed878b834f28fb9fbf10bf122341c0fae15d69
Victor Willian <victorwillian1012@gmail.com>
2021-01-16 02:11:53 -0300

    C++ challenges

---
+++ c++/sum/sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ostream>
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    double a, b, c;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
+
+    std::cout << a + b + c << std::endl;
+    return 0;
+}
+
