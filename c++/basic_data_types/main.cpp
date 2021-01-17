c4c6af8712e033be893e095e513283594edf3a88
Victor Willian <victorwillian1012@gmail.com>
2021-01-17 00:34:36 -0300

    Basic data types challenge

---
+++ c++/basic_data_types/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <ostream>
+#include <stdio.h>
+#include <float.h>
+
+int main() {
+    // Complete the code.
+
+    int valor1;
+    long valor2;
+    char valor3;
+    float valor4;
+    double valor5;
+
+    std::cin >> valor1;
+    std::cin >> valor2;
+    std::cin >> valor3;
+    std::cin >> valor4;
+    std::cin >> valor5;
+
+    printf("%d\n", valor1);
+    printf("%ld\n", valor2);
+    printf("%c\n", valor3);
+    printf("%f\n", valor4);
+    printf("%lf\n", valor5);
+
+
+    return 0;
+}
